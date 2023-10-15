8a3151d1bd40243a938e207cfd5cd78f4ccd5668
iou1006 <iou1006@hotmail.com>
2023-10-15 16:18:36 +0800

    Initial commit

---
+++ pages/index/index.js
@@ -0,0 +1,20 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    canIUseGetUserProfile: false,
+    canIUseOpenData: true
+  },
+  // 事件处理函数
+  bindViewTap() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad() {
+  }
+})
